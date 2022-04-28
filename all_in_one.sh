998e76b7939cc183ca233f3919dd6de3a7764d93
jclayton09 <82755412+jclayton09@users.noreply.github.com>
2022-04-28 19:57:59 +0100

    bash file
    file was already run / debug and commented

---
+++ all_in_one.sh
@@ -0,0 +1,35 @@
+#! /usr/bin/bash
+
+# sudo apt-get install sshpass
+
+PASSWORD=$3
+USERNAME=$2
+IP=$3
+
+TIME=$(sshpass -p ${PASSWORD} ssh ${USERNAME}@${IP} 'date +"%Y-%m-%d %H:%M:%S"')
+
+sshpass -p ${PASSWORD} ssh ${USERNAME}@${IP} "echo $PASSWORD | sudo -S timedatectl set-timezone UTC"
+echo "It's a magic password"
+eval "sudo timedatectl set-ntp false"
+# command to be able to set the time (disbale automatic sync)
+eval "sudo timedatectl set-timezone UTC"
+# sets the timezone to UTC
+
+if [ -z "$TIME" ]
+# checks if the variable was assigned a time
+then
+echo "Couldn't find the new time"
+else
+# echo $TIME
+echo 'Setting the new time to:'
+eval "sudo date -s '${TIME}'"
+# command to set the date to the variable
+fi
+
+x-terminal-emulator --title='CPU' --command="sshpass -p ${PASSWORD} ssh ${USERNAME}@${IP} 'echo ${PASSWORD} | sudo -S Unified_Server_PTU_V3.6.1_20211216/ptu -ct 1'"
+# x-terminal-emulater brings up another command window named CPU
+# sshpass enters the password in for you
+# echo + sudo -S is how to enter the password from stdin
+
+x-terminal-emulator --title='GPU' --command="sshpass -p ${PASSWORD} ssh ${USERNAME}@${IP} 'echo ${PASSWORD} | sudo -S bash gpu-burn.sh'"
+
